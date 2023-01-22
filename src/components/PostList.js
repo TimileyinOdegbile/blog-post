f3c4d64488ea0993a92f95fef9a55127006bb4e3
Timileyin Odegbile <odegbiletimileyin11@gmail.com>
2023-01-22 16:03:42 +0100

    Async action with redux thunk

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 class PostList extends React.Component {
-    render() {
-        return <div>PostList</div>
-    }
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return <div>PostList</div>;
+  }
 };
 
-export default PostList;
+export default connect(null, { fetchPosts })(PostList);
